ffc150cf7fd1c39b82551d86a33128f010e355e2
Glitch (obsidian-canoe) <none>
2019-11-16 02:29:53 +0000

    🗼👞 Checkpoint ./routes/api.js:7351991/484

--- routes/api.js
+++ routes/api.js
@@ -16,7 +16,7 @@ module.exports = function (app, db) {
   app.route('/api/stock-prices')
     .get(function (req, res){
       //res.send(req.headers["x-forwarded-for"].match(/(?:[0-9]{1,3}\.){3}[0-9]{1,3}/)[0])
-    res.send(req.query)
+    //$.json('https://finance.google.com/finance/info?q=NASDAQ%3aMSFT', data=>{res.json(data)})
     });
     
 };

23658f8b9b55e2f1243aeef73667237410191239
Glitch (obsidian-canoe) <none>
2019-11-16 01:54:55 +0000

    📅👷 Checkpoint ./routes/api.js:7351991/111 ./server.js:7351991/1006 ./package.json:7351991/11423

--- routes/api.js
+++ routes/api.js
@@ -11,9 +11,7 @@
 var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
-
-module.exports = function (app) {
+module.exports = function (app, db) {
 
   app.route('/api/stock-prices')
     .get(function (req, res){
--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+var express       = require('express');
+var bodyParser    = require('body-parser');
+var expect        = require('chai').expect;
+var cors          = require('cors');
+const helmet      = require('helmet');
+const MongoClient = require('mongodb').MongoClient
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
 var app = express();
+
+app.use(helmet({
+  contentSecurityPolicy: {directives: {
+    defaultSrc: ["'self'"]
+  }}
+}))
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -18,24 +26,31 @@ app.use(cors({origin: '*'})); //For FCC testing purposes only
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+MongoClient.connect(process.env.DB, {useUnifiedTopology: true}, function(err, client){
+  
+  //Index page (static HTML)
+  app.route('/')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/index.html');
+    });
+  
+  //404 Not Found Middleware
+  app.use(function(req, res, next) {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
   });
+  
+  //Routing for API 
+  apiRoutes(app, client.db('stock_prices'));
+
+})
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
-apiRoutes(app);  
+
     
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
-});
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {

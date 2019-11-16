8426b3969a6c46838c02e0f0b10f12d5dfc8e0e4
Glitch (obsidian-canoe) <none>
2019-11-16 02:17:57 +0000

    ⛈🐚 Checkpoint ./routes/api.js:7351991/25

--- routes/api.js
+++ routes/api.js
@@ -16,7 +16,7 @@ module.exports = function (app, db) {
   app.route('/api/stock-prices')
     .get(function (req, res){
       //res.send(req.headers["x-forwarded-for"].match(/(?:[0-9]{1,3}\.){3}[0-9]{1,3}/)[0])
-    res.json(req.data)
+    res.send(req.query)
     });
     
 };

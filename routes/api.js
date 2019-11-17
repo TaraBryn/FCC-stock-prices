8d4bb965f7cd6922f1c99b91591270b2c0088d0d
Glitch (obsidian-canoe) <none>
2019-11-17 02:49:26 +0000

    🐋🚔 Checkpoint ./routes/api.js:7351991/1739

--- routes/api.js
+++ routes/api.js
@@ -20,18 +20,18 @@ module.exports = function (app, db) {
     var stocks = req.query.stock;
     if (!Array.isArray(stocks)) stocks = [stocks];
     if(stocks.length > 2) stocks.splice(2);
+    
     Promise.all(stocks.map(e=>{
       var url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${e}&apikey=${process.env.API_KEY}`;
       var dataReq = new XMLHttpRequest();
       dataReq.open('GET', url, true);
-      dataReq.onload=function(){
+      return dataReq.onload=()=>Promise(function(){
         var rawData = JSON.parse(dataReq.responseText);
         var data = rawData['Time Series (Daily)'];
         var keys = Object.keys(data).sort((a,b)=>a-b);
         return data[keys[0]];
-      }
-      var promise = new Promise(dataReq.send);
-      return (promise.then(d=>d))
+      }).then(data=>data)
+      return 
     }))
     .then(data => {
       console.log(data);

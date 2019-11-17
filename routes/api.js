6f4a1aecdc9a0018c47f5d234423c84926fc2697
Glitch (obsidian-canoe) <none>
2019-11-17 01:02:25 +0000

    🌼👶 Checkpoint ./routes/api.js:7351991/46

--- routes/api.js
+++ routes/api.js
@@ -18,13 +18,15 @@ module.exports = function (app, db) {
     var stocks = req.query.stock;
     if (!Array.isArray(stocks)) stocks = [stocks];
     if(stocks.length > 2) stocks.splice(2);
-    Promise.all(stocks.map(e=>{
+    Promise.all(stocks.forEach(e=>{
       var url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${e}&apikey=${process.env.API_KEY}`;
       var dataReq = new XMLHttpRequest();
       dataReq.open('GET', url, true);
       dataReq.send();
       dataReq.onload=function(){
         var data = JSON.parse(dataReq.responseText);
+        console.log(data);
+        
       }
     }))
     .then(data => {
